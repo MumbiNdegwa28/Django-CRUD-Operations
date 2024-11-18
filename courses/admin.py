87420962f28ac8220919ebbcecd1fedd947ce802
MumbiNdegwa <mumbi.ndegwa20@gmail.com>
2024-11-18 22:11:02 +0300

    view data page and django admin connection

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CourseRegistration
+
+@admin.register(CourseRegistration)
+class CourseRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'academic_documents')
+    search_fields = ('name', 'email', 'phone')
